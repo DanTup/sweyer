8484fc36525fed10c01ab5ed51bd53f22aae423d
nt4f04uNd <nt4f04uNd@gmail.com>
2021-05-18 20:25:31 +0300

    breakpoint is not hit

--- android/app/src/main/java/com/example/sweyer/MainActivity.java
+++ android/app/src/main/java/com/example/sweyer/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.sweyer;
 
+import android.content.Context;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import com.ryanheise.audioservice.AudioServicePlugin;
 
-public class MainActivity extends FlutterActivity { }
+public class MainActivity extends FlutterActivity {
+   @Override
+   public FlutterEngine provideFlutterEngine(Context context) {
+      return AudioServicePlugin.getFlutterEngine(context);
+   }
+}
